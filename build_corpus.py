c422006c95f6eeabd97889f39dbb6e558efe3626
David Kirkby <dkirkby@uci.edu>
2016-12-31 16:05:56 -0800

    Save wikipedia content for each topic word

--- build_corpus.py
+++ build_corpus.py
@@ -2,17 +2,11 @@
 from __future__ import print_function, division
 
 import argparse
+import io
+import os.path
 
 import wikipedia
 
-# Himalayas -> no disambiguation page found
-# Ice Cream -> no disambiguation page found
-# Limousine -> no disambiguation page found
-# Microscope -> no disambiguation page found
-# Pants -> no disambiguation page found
-# Ruler -> no disambiguation page found
-# Shoe -> show
-# Stream -> street
 
 def main():
     parser = argparse.ArgumentParser(
@@ -20,35 +14,55 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input', type=str, default='words.txt',
                         help='Name of word list to use.')
+    parser.add_argument('--encoding', type=str, default='utf8',
+                        help='Encoding for saving corpus text.')
     args = parser.parse_args()
 
+    # Read the word list into memory and format using wikimedia conventions.
+    # https://en.wikipedia.org/wiki/Wikipedia:Naming_conventions_(capitalization)
     with open(args.input, 'r') as f:
-        words = [w.strip() for w in f]
+        words = [w.strip().capitalize() for w in f]
     print('Read {0} words from {1}.'.format(len(words), args.input))
 
     for word in words:
-        word = ' '.join([w.capitalize() for w in word.split()])
+        if word < 'Head': continue
         print(word)
-        # Lookup the disambiguation pages for this topic.
-        page_name = word + ' (disambiguation)'
         try:
+            # Look for a disambiguation page on this topic.
             page = wikipedia.page(
-                page_name, preload=False, redirect=True, auto_suggest=False)
-            # Assume that we have re-directed to a non-disambiguation page.
-            pages = [page.title]
+                word + ' (disambiguation)', preload=False,
+                redirect=True, auto_suggest=False)
+            # Assume that we have re-directed to a non-disambiguation page
+            # if we get here (ie, with no DisambiguationError raised).
+            titles = [page.title]
         except wikipedia.exceptions.PageError as e:
-            # Redirect page does not exist so use topic directly.
-            pages = [word]
+            # Disambiguation page does not exist and does not redirect
+            # so assume that there is a unique topic page.
+            print('-> unique topic page')
+            titles = [unicode(word)]
         except wikipedia.exceptions.DisambiguationError as e:
-            pages = e.options
+            # Record the list of pages related to this topic.
+            titles = e.options
+
         num_pages = 0
-        for page in pages:
-            # Skip second-level disambiguation pages.
-            if page.endswith(' (disambiguation)'):
-                continue
-            #print('  ' + page)
-            num_pages += 1
-        print('  {0}'.format(num_pages))
+        out_name = os.path.join('corpus', word + '.txt')
+        with io.open(out_name, 'w', encoding=args.encoding) as out:
+            for title in titles:
+                # Remove any quotes.
+                title = title.translate({ord('"'): None})
+                print('  {0:3d} {1}'.format(
+                    num_pages, title.encode('ascii', 'replace')))
+                try:
+                    page = wikipedia.page(
+                        title, auto_suggest=False, redirect=True)
+                    out.write(page.content)
+                    num_pages += 1
+                except wikipedia.exceptions.DisambiguationError as e:
+                    # Skip second-level ambiguous topics.
+                    print('.. ambiguous')
+                except Exception as e:
+                    print('!! ' + str(e))
+            print('  saved {0} pages to {1}'.format(num_pages, out_name))
 
 
 if __name__ == '__main__':

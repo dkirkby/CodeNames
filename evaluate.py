786bc833e2bdf393493203e3da5ed871566af528
David Kirkby <dkirkby@uci.edu>
2017-01-03 11:37:26 -0800

    Add pairs suite and save plots

--- evaluate.py
+++ evaluate.py
@@ -16,7 +16,17 @@ def main():
                         help='Name of word list to use.')
     parser.add_argument('--top-singles', type=int, default=10,
                         help='Show top single matches.')
+    parser.add_argument('--top-pairs', type=int, default=0,
+                        help='Show top pair matches.')
+    parser.add_argument('--save-plots', type=str, default=None,
+                        help='Save plots using this filename root.')
     args = parser.parse_args()
+
+    if args.save_plots:
+        # Only import if needed and use background plotting.
+        import matplotlib
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
 
     embedding = model.WordEmbedding(args.input)
 
@@ -38,6 +48,40 @@ def main():
             clues = [w for w,s in saved_clues[i][1]]
             print('{0:.3f} {1} = {2}'.format(
                 best_score[i], word.upper(), ', '.join(clues)))
+        if args.save_plots:
+            plt.hist(best_score, range=(0., 1.), bins=50)
+            plt.xlim(0., 1.)
+            plt.xlabel('Similarity Score')
+            plt.ylabel('Singles')
+            plt.yscale('log')
+            plt.grid()
+            plt.savefig(args.save_plots + '_singles.png')
+
+    if args.top_pairs > 0:
+        best_score, saved_clues = [], []
+        for i1, word1 in enumerate(words):
+            for i2, word2 in enumerate(words[:i1]):
+                clues = embedding.get_clues((word1, word2), (word1, word2))
+                if clues:
+                    best_score.append(clues[0][1])
+                    saved_clues.append(((i1, i2), clues))
+        num_clues = len(saved_clues)
+        order = sorted(
+            xrange(num_clues), key=lambda k: best_score[k], reverse=True)
+        for i in order[:args.top_pairs]:
+            i1, i2 = saved_clues[i][0]
+            clues = [w for w,s in saved_clues[i][1]]
+            print('{0:.3f} {1} + {2} = {3}'.format(
+                best_score[i], words[i1].upper(), words[i2].upper(),
+                ', '.join(clues)))
+        if args.save_plots:
+            plt.hist(best_score, range=(0., 1.), bins=50)
+            plt.xlim(0., 1.)
+            plt.xlabel('Similarity Score')
+            plt.ylabel('Pairs')
+            plt.yscale('log')
+            plt.grid()
+            plt.savefig(args.save_plots + '_pairs.png')
 
 
 if __name__ == '__main__':
